376a433f3a32b1414dcf9594bf80ce6e376f4a18
user <sami.elnakadi@gmail.com>
2019-03-14 16:56:39 +0100

    partie1a

--- challenge-js-files/assets/graph.js
+++ challenge-js-files/assets/graph.js
@@ -1,41 +1,24 @@
-let titre = document.getElementById('Crimes_et_d.C3.A9lits_enregistr.C3.A9s_par_les_services_de_police');
-titre.innerHTML += '\n' + " HEllo";
-let table1 = document.getElementById('table1');
-let jso = JSON.stringify(table1);
-console.log(jso);
+function addDiv(){
+    let newDiv = document.createElement("div")
+    newDiv.id = "graph"
+}
+let tableth = document.querySelectorAll('th')
+let tabletd = document.querySelectorAll('td')
 
-let titre2=document.getElementById('Homicide');
-
-
-
-//Partie2
-
-const req = new XMLHttpRequest;
-req.open('GET','https://inside.becode.org/api/v1/data/random.json',true);
-req.onload = function(){
-    if(this.status===404){
-        alert('An error occured')
-    }
-    else if(this.status===200){
-        console.log(JSON.parse(this.responseText));
-        
-        
-    }
-    
+for (let i = 0; i<tableth.length; i++){
+    let th = tableth[i].innerHTML
+    console.log(th)
 }
 
-req.send();   
+for (let j = 0; j<tabletd.length; j++){
+    let td = tabletd[j].innerHTML
+    console.log(td)
+}
 
-var svg = dimple.newSvg("#chartContainer", 590, 400);
-    d3.tsv("https://inside.becode.org/api/v1/data/random.json", function (data) {
-      data = dimple.filterData(data, "Owner", ["Aperture", "Black Mesa"])
-      var myChart = new dimple.chart(svg, data);
-      myChart.setBounds(60, 30, 430, 330);
-      var x = myChart.addCategoryAxis("x", ["Owner", "Month"]);
-      x.addGroupOrderRule("Date");
-      myChart.addMeasureAxis("y", "Unit Sales");
-      var s = myChart.addSeries(["Brand"], dimple.plot.line);
-      s.barGap = 0.05;
-      myChart.addLegend(510, 20, 100, 300, "left");
-      myChart.draw();
-    });
+// let titre = document.getElementById('Crimes_et_d.C3.A9lits_enregistr.C3.A9s_par_les_services_de_police');
+// titre.innerHTML += '\n' + " HEllo";
+// let table1 = document.getElementById('table1');
+// let jso = JSON.stringify(table1);
+// console.log(jso);
+
+// let titre2=document.getElementById('Homicide');
